13b7d76c26f20b7a67b5c37fe47eac5ee06df4ea
Giliard Arruda <giliardarruda.dev@gmail.com>
2024-07-11 20:25:55 -0300

    Update teste.cy.js

--- cypress/e2e/teste.cy.js
+++ cypress/e2e/teste.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('[data-test="username"]').type("standard_user")
+    cy.get('[data-test="password"]').type("secret_sauce")
+    cy.get('#login-button').click()
   })
 })
